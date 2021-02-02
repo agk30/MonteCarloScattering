7cd999e71560be1c2789105da9e7b5f2eca5381f
Adam <adamknight1992@gmail.com>
2021-02-02 13:16:29 +0000

    ROI analysis update
    Now produces nicely formatted .csv files for each angle for easy import into origin.

--- Testing and Analysis/ROI Analysis/roi.f90
+++ Testing and Analysis/ROI Analysis/roi.f90
@@ -2,12 +2,12 @@ program roiAnalysis
     implicit none
 
     double precision, dimension(420,420) :: image
-    double precision, dimension(-4:4,-4:4,5,5,40) :: roi
+    double precision, allocatable, dimension(:,:,:,:,:) :: roi
     double precision, dimension(2) :: centrePx, roiCentre
     double precision, dimension(5) :: roiRadius
     double precision, dimension(5) :: angle
-    integer :: i, j, k, l, m, startImg, numimg, roiSize
-    character(100) :: filename
+    integer :: i, j, radii, angles, m, startImg, numimg, roiSize, numRoi, numAngles
+    character(100) :: filename, c, roiNumber
 
     centrePx(1) = 210.0 ; centrePx(2) = 294.0
     roiRadius(1) = 50.0 ; roiRadius(2) = 60.0 ; roiRadius(3) = 70.0 ; roiRadius(4) = 80.0 ; roiRadius(5) = 90.0
@@ -17,77 +17,71 @@ program roiAnalysis
 
     angle = angle*((2D0*3.141592653589793D0)/360D0)
 
-    startImg = 150
+    startImg = 50
 
-    numimg = 50
+    numimg = 40
 
-    roiSize = 3
+    ! (length of roi box - 1) / 2
+    roiSize = 4
+
+    numRoi = 5
+
+    numAngles = 5
+
+    allocate(roi(-roiSize:roiSize,-roiSize:roiSize,numRoi,numAngles,numimg))
  
     do i = 1, numimg
         if ((i+startImg) .lt. 100) then
-            write(filename,'("Images/Image",I3,".txt")') (i+startImg)
+            write(filename,'("../../Images2/Image",I3,".txt")') (i+startImg)
         else
-            write(filename,'("Images/Image",I3,".txt")') (i+startImg)    
+            write(filename,'("../../Images2/Image",I3,".txt")') (i+startImg)    
         end if
         open(10+i,file=trim(filename))
-        !print *, trim(filename)
     end do
-    
 
-
-    open(500,file='roidata.txt')
-
+    do i = 1, numAngles
+        write(roiNumber,'(I2)') i
+        open(500+i,file='roidata'//trim(roiNumber)//'.csv')
+    end do
 
     ! loops through images
     do m = 1, numimg
 
-        do i = 1, 420
-            
+        do i = 1, 420    
             read(10+m,*) (image(j,i),j=1,420)
-
-            !print *, sum(image)
-
         end do
 
         ! loop through angles
-        do l = 1, 5
-
+        do angles = 1, 5
             ! loop through radii
-            do k = 1, 5
-
-                roiCentre(1) = floor(sin(angle(l))*roiRadius(k))
-                roiCentre(2) = floor(cos(angle(l))*roiRadius(k))
-
+            do radii = 1, 5
+                roiCentre(1) = floor(sin(angle(angles))*roiRadius(radii))
+                roiCentre(2) = floor(cos(angle(angles))*roiRadius(radii))
                 do i = -roiSize, roiSize   
                     do j = -roiSize, roiSize
-                        roi(i,j,k,l,m) = image((int(centrePx(1)) + int(roiCentre(1)) + i),&
+                        roi(i,j,radii,angles,m) = image((int(centrePx(1)) + int(roiCentre(1)) + i),&
                          (int(centrePx(2)) - int(roiCentre(2) + j)))
-
-                         !print *, image((int(centrePx(1)) + int(roiCentre(1)) + i), (int(centrePx(2)) - int(roiCentre(2) + j)))
                     end do
                 end do
-
             end do
-
         end do
-
     end do
-    do k = 1, 5
 
-        do j = 1, 5
-
-            write(500,*)  "Angle", angle(k), "ROI", j
-
-            do i = 1, numimg
-                print *, numimg
-
-                ! j for radius, k for angle
-                write(500,*) SUM(roi(:,:,j,k,i))
-
-            end do
-
-        end do 
     
+
+    do angles = 1, numAngles
+        write(500+angles,*) "ROI 1,","ROI 2,","ROI 3,","ROI 4,","ROI 5"
+            do i = 1, numimg
+                do radii = 1, numRoi
+                    write(500+angles,'(ES12.5)',advance='no') SUM(roi(:,:,radii,angles,i))
+                    if (radii .lt. numRoi) then
+                        write(500+angles,'(a)',advance='no') ","
+                    else
+                        write(500+angles,'(a)',advance='no') new_line(c)
+                    end if
+                end do
+            end do
     end do
+
 
 end program roiAnalysis
