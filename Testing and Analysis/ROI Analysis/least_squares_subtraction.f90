e444ce6143a3cc754f85524ae5e02eb5a8d2e336
Adam <agk30@hw.ac.uk>
2021-05-25 13:20:35 +0100

    least squares update

--- Testing and Analysis/ROI Analysis/least_squares_subtraction.f90
+++ Testing and Analysis/ROI Analysis/least_squares_subtraction.f90
@@ -1,83 +1,85 @@
-program lss
+program find_factor
     implicit none
 
-    integer :: i, array_size, bracket_index
-    double precision, allocatable, dimension(:) :: data_array, background_array
-    double precision, dimension(3) :: bracket, sum_square_initial
-    double precision :: tolerance, residual, upper, lower
-    double precision :: current_min, prev_min, sum_square_lower, sum_square_upper
+    double precision, dimension(5) :: test_data, test_background
+    double precision :: factor, tolerance, lower, upper
 
-    allocate(data_array(5))
-    allocate(background_array(5))
+    tolerance = 1E-6
 
-    data_array = [1.0, 2.0, 3.0, 4.0, 5.0]
-    background_array = [1.1, 2.2, 3.0, 2.5, 4.0]
+    upper = 1.5
+    lower = 0.5
 
-    array_size = SIZE(data_array)
+    test_data = [1.0, 2.0, 3.0, 4.0, 5.0]
+    test_background = [1.0, 2.0, 3.0, 4.0, 5.0]
 
-    bracket(3) = 1.5
-    bracket(1) = 0.5
+    call least_squares_fit(test_data(:), test_background(:), lower, upper, tolerance, factor)
 
-    bracket(2) = bracket(3) - ((bracket(3) - bracket(1)) / 2.0)
+    print *, factor
 
-    tolerance = 1E-4
+    contains
 
-    sum_square_lower = 0
-    sum_square_upper = 0
-
-    prev_min = 50.0
-
-    lower = bracket(2) - ((bracket(2) - bracket(1)) / 2.0)
-    upper = bracket(3) - ((bracket(3) - bracket(2)) / 2.0)
-
-    do bracket_index = 1, 3
-        do i = 1, array_size
-            residual = data_array(i) - (background_array(i)*bracket(bracket_index))
-
-            sum_square_initial(bracket_index) = sum_square_initial(bracket_index) + residual**2.0
-        end do
-    end do
-
-    print *, sum_square_initial
-
-    do
-        sum_square_lower = 0
-        sum_square_upper = 0
-
-        do i = 1, array_size
-            sum_square_lower = sum_square_lower + ((data_array(i) - (lower*background_array(i)))**2.0)
-            sum_square_upper = sum_square_upper + ((data_array(i) - (upper*background_array(i)))**2.0)
-        end do
-
-        if (sum_square_lower .lt. sum_square_upper) then
-            prev_min = current_min
-            current_min = lower
-
-            if (abs(prev_min - current_min) .lt. tolerance) then
-                print *, lower
-                EXIT
-            else
-                bracket(3) = bracket(2)
-                bracket(2) = bracket(3) - ((bracket(3) - bracket(1))/2.0)
-
-                upper = bracket(3) - ((bracket(3) - bracket(2))/2.0)
-                lower = bracket(2) - ((bracket(2) - bracket(1))/2.0)
-            end if
-        else
-            prev_min = current_min
-            current_min = upper
-            if (abs(prev_min - current_min) .lt. tolerance) then
-
-                print *, upper
-                EXIT
-
-            else
-                bracket(1) = bracket(2)
-                bracket(2) = bracket(3) - ((bracket(3) - bracket(1))/2.0)
-
-                upper = bracket(3) - ((bracket(3) - bracket(2))/2.0)
-                lower = bracket(2) - ((bracket(2) - bracket(1))/2.0)
-            end if
-        end if
-    end do
-end program lss
+        subroutine least_squares_fit (data_array, background_array, low_limit, high_limit, tolerance, factor)
+            implicit none
+        
+            integer :: i, array_size, bracket_index
+            double precision, intent(in) :: low_limit, high_limit
+            double precision, intent(out) :: factor, tolerance
+            double precision, intent(in), dimension(:) :: data_array, background_array
+            double precision, dimension(3) :: bracket, sum_square_initial
+            double precision :: current_min, prev_min, sum_square_lower, sum_square_upper, upper, lower
+        
+            array_size = SIZE(data_array)
+        
+            bracket(3) = high_limit
+            bracket(1) = low_limit
+        
+            bracket(2) = bracket(3) - ((bracket(3) - bracket(1)) / 2.0)
+        
+            sum_square_lower = 0
+            sum_square_upper = 0
+        
+            prev_min = 50.0
+        
+            lower = bracket(2) - ((bracket(2) - bracket(1)) / 2.0)
+            upper = bracket(3) - ((bracket(3) - bracket(2)) / 2.0)
+        
+            do
+                sum_square_lower = 0
+                sum_square_upper = 0
+        
+                do i = 1, array_size
+                    sum_square_lower = sum_square_lower + ((data_array(i) - (lower*background_array(i)))**2.0)
+                    sum_square_upper = sum_square_upper + ((data_array(i) - (upper*background_array(i)))**2.0)
+                end do
+        
+                if (sum_square_lower .lt. sum_square_upper) then
+                    prev_min = current_min
+                    current_min = lower
+        
+                    if (abs(prev_min - current_min) .lt. tolerance) then
+                        factor = lower
+                        EXIT
+                    else
+                        bracket(3) = bracket(2)
+                        bracket(2) = bracket(3) - ((bracket(3) - bracket(1))/2.0)
+        
+                        upper = bracket(3) - ((bracket(3) - bracket(2))/2.0)
+                        lower = bracket(2) - ((bracket(2) - bracket(1))/2.0)
+                    end if
+                else
+                    prev_min = current_min
+                    current_min = upper
+                    if (abs(prev_min - current_min) .lt. tolerance) then
+                        factor = upper
+                        EXIT
+                    else
+                        bracket(1) = bracket(2)
+                        bracket(2) = bracket(3) - ((bracket(3) - bracket(1))/2.0)
+        
+                        upper = bracket(3) - ((bracket(3) - bracket(2))/2.0)
+                        lower = bracket(2) - ((bracket(2) - bracket(1))/2.0)
+                    end if
+                end if
+            end do
+        end subroutine least_squares_fit
+end program find_factor
